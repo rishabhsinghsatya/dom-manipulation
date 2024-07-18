018d1f8084890e7fee5739d8f0ea8c17ebaa16ce
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-18 16:29:06 +0530

    complete NF clone

---
+++ P6-NetflixHP clone/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".faq-question").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const answer = item.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+    item.classList.toggle("active");
+  });
+});
